a1e99e6f0daaa9cd531d13d25df113ddfa3e343f
javaturk <akin@javaturk.org>
2020-11-26 19:49:10 +0300

    Update SelamTest.java

--- src/org/javaturk/oopj/ch01/SelamTest.java
+++ src/org/javaturk/oopj/ch01/SelamTest.java
@@ -1,3 +1,23 @@
+
+package org.javaturk.oopj.ch01;
+
 /**
 * Client of Selam class.
 */
+public class SelamTest {
+
+	public static void main(String[] args) {
+		Selam nesne = new Selam();
+
+		String cevap = nesne.selamSoyle("Ali");
+		System.out.println(cevap);
+		
+		cevap = nesne.selamSoyle("Zeynep");
+		System.out.println(cevap);
+		
+		System.out.println("World: " + nesne.world);
+		
+		cevap = nesne.selamSoyle("");
+		System.out.println(cevap);
+	}
+}
