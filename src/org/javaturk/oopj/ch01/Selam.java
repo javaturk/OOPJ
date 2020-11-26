84dc0f97330f6ee15738db3243f3aa9cb2a885df
javaturk <akin@javaturk.org>
2020-11-26 19:48:20 +0300

    Update Selam.java

--- src/org/javaturk/oopj/ch01/Selam.java
+++ src/org/javaturk/oopj/ch01/Selam.java
@@ -1,3 +1,20 @@
+
+package org.javaturk.oopj.ch01;
+
 /**
 * Selam.java
 */
+public class Selam{
+	
+	String world = "millet"; 
+	
+	public String selamSoyle(String kime) {
+		String cumle;
+		if (kime != "")
+			cumle = "Selam " + kime + " :)";
+		else
+			cumle = "Selam " + world + " :)";
+		return cumle;
+  }
+} 
+
